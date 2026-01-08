e8b1c297308c12abaf2d5637cc203279e272abae
Kaelem Chandra <kc@kaelem.net>
2026-01-08 15:27:38 +0800

    Clone playground & image bundle during build time

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -7,18 +7,7 @@ free -m
 sudo sysctl -w fs.inotify.max_user_watches=1048576
 sudo sysctl -w fs.inotify.max_user_instances=512
 
-git clone "https://github.com/$EDA_PLAYGROUND_REPO" $EDA_PLAYGROUND_DIR
-
-curl -o /tmp/bundle.yaml "https://raw.githubusercontent.com/nokia-eda/edaadm/refs/heads/main/bundles/eda-bundle-core-$EDA_VERSION.yaml"
-
-cd $EDA_PLAYGROUND_DIR
-make download-tools
-
-echo "export PATH=$PATH:$EDA_PLAYGROUND_DIR/tools" >> $HOME/.zshrc
-
-
-$EDA_PLAYGROUND_DIR/tools/yq '.assets.registries[] | .name as $reg | .images[] | .name as $img | .tags[] | $reg + "/" + $img + ":" + .' /tmp/bundle.yaml > $HOME/.images.txt
-TOKEN=$($EDA_PLAYGROUND_DIR/tools/yq -o=json '.assets.registries[].auth' /tmp/bundle.yaml | jq -r '(reduce range(.extraEncodeCount + 1) as $_ (.username; @base64d)) + ":" + (reduce range(.extraEncodeCount + 1) as $_ (.password; @base64d))')
+TOKEN=$($EDA_PLAYGROUND_DIR/tools/yq -o=json '.assets.registries[].auth' $HOME/.bundle.yaml | jq -r '(reduce range(.extraEncodeCount + 1) as $_ (.username; @base64d)) + ":" + (reduce range(.extraEncodeCount + 1) as $_ (.password; @base64d))')
 
 ensure-docker-is-ready
 
