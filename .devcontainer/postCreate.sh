20a04bb0dd3f83086e6174f91ff42d60edb92d62
Kaelem Chandra <kc@kaelem.net>
2026-01-09 21:38:55 +0800

    Add custom kpt setters file invocation

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,6 +5,6 @@ cd $EDA_PLAYGROUND_DIR
 
 ensure-docker-is-ready
 
-make try-eda NO_KIND=yes NO_LB=yes
+make try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=$EDA_PLAYGROUND_DIR/configs/codespaces-4vcpu-kpt-setters.yaml
 
 make configure-codespaces-keycloak
