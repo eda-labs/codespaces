f2772f0572f90b5750df62a04ff5e72ae9139d17
Roman Dodin <dodin.roman@gmail.com>
2026-01-09 17:41:34 +0100

    Debian:12 for base image, build pipeline and prefetch of srl image (#1)
    * use bookworm
    
    * added zsh and tools path to zsh
    
    * rename base image
    
    * Add the SRL image to the pre pull list
    
    * added build workflow
    
    * assuming context
    
    * explicit context
    
    * im dumb
    
    * default perms
    
    * use image name
    
    * use perms
    
    * use repo image
    
    * use all ref tags
    
    * sigh
    
    * Add custom kpt setters file invocation
    
    * Remove duplicate keycloak config step after try-eda executes
    
    * host codespaces setters
    
    * Open EDA UI in new tab on port auto forward
    
    * use non tmp path
    
    ---------
    
    Co-authored-by: Kaelem Chandra <kc@kaelem.net>

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,6 +5,4 @@ cd $EDA_PLAYGROUND_DIR
 
 ensure-docker-is-ready
 
-make try-eda NO_KIND=yes NO_LB=yes
-
-make configure-codespaces-keycloak
+make try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=/eda-codespaces/codespaces-4vcpu-kpt-setters.yaml
