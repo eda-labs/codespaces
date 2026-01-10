8f2c5fbef031ddb627528be322cafe7a5c985bdc
hellt <dodin.roman@gmail.com>
2026-01-10 15:55:11 +0100

    reduce to 90s

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,5 +5,5 @@ cd $EDA_PLAYGROUND_DIR
 
 ensure-docker-is-ready
 
-# Run make with 180s inactivity timeout, retry up to 3 times
-run-with-inactivity-timeout 180 3 make try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=/eda-codespaces/codespaces-4vcpu-kpt-setters.yaml
+# Run make with 90s inactivity timeout, retry up to 3 times
+run-with-inactivity-timeout 90 3 make try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=/eda-codespaces/codespaces-4vcpu-kpt-setters.yaml
