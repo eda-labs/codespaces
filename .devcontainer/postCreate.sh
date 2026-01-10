32de4477a42b2523a9818027e3b425b33a65dd0d
hellt <dodin.roman@gmail.com>
2026-01-10 21:34:07 +0100

    fix path for images

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,6 +5,6 @@ cd $EDA_PLAYGROUND_DIR
 
 ensure-docker-is-ready
 
-docker exec k3d-eda-demo-server-0 sh -c "cat /opt/images.txt | xargs -P $NUM_CONCURRENT_IMG_PREPULLS -I {} crictl pull --creds $TOKEN {}"
+docker exec k3d-eda-demo-server-0 sh -c "cat /home/vscode/images.txt | xargs -P $NUM_CONCURRENT_IMG_PREPULLS -I {} crictl pull --creds $TOKEN {}"
 
 make -f Makefile -f /workspaces/codespaces/.devcontainer/overrides.mk try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=$TRY_EDA_KPT_SETTERS_FILE
