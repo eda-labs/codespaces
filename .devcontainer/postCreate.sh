d903379fe71f47ccf2a355f8b33374e7a3f78c38
hellt <dodin.roman@gmail.com>
2026-01-10 13:59:15 +0100

    60s wait time

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,4 +6,4 @@ cd $EDA_PLAYGROUND_DIR
 ensure-docker-is-ready
 
 # Run make with 30s inactivity timeout, retry up to 5 times
-run-with-inactivity-timeout 30 5 make try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=/eda-codespaces/codespaces-4vcpu-kpt-setters.yaml
+run-with-inactivity-timeout 60 5 make try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=/eda-codespaces/codespaces-4vcpu-kpt-setters.yaml
