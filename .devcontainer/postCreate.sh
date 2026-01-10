038334fc0dec14805122f647cd322836d9b10957
Kaelem Chandra <kc@kaelem.net>
2026-01-10 22:29:08 +0800

    Use correct img, try without timeout wrapper

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,4 +6,4 @@ cd $EDA_PLAYGROUND_DIR
 ensure-docker-is-ready
 
 # Run make with 180s inactivity timeout, retry up to 3 times
-run-with-inactivity-timeout 180 3 make try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=/eda-codespaces/codespaces-4vcpu-kpt-setters.yaml
+make try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=/eda-codespaces/codespaces-4vcpu-kpt-setters.yaml
