fbac5b6905bbca1e2f7d746321809f8b040e236c
Kaelem Chandra <kc@kaelem.net>
2026-01-12 00:13:55 +0800

    Set EXT_DOMAIN_NAME

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -9,4 +9,4 @@ ensure-docker-is-ready
 TOKEN=$($EDA_PLAYGROUND_DIR/tools/yq -o=json '.assets.registries[].auth' $HOME/.bundle.yaml | jq -r '(reduce range(.extraEncodeCount + 1) as $_ (.username; @base64d)) + ":" + (reduce range(.extraEncodeCount + 1) as $_ (.password; @base64d))')
 docker exec k3d-eda-demo-server-0 sh -c "cat /opt/images.txt | xargs -P $(nproc) -I {} crictl pull --creds $TOKEN {}"
 
-make -f Makefile -f $TRY_EDA_OVERRIDES_FILE try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=$TRY_EDA_KPT_SETTERS_FILE
+make -f Makefile -f $TRY_EDA_OVERRIDES_FILE try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=$TRY_EDA_KPT_SETTERS_FILE EXT_DOMAIN_NAME="$CODESPACE_NAME-9443.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"
