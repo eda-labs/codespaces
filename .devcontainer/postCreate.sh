4eab4ee22393a538dd47f912604b135ecbb4b1ac
hellt <dodin.roman@gmail.com>
2026-01-12 20:43:28 +0100

    use LB with kind

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -13,4 +13,4 @@ TOKEN=$($EDA_PLAYGROUND_DIR/tools/yq -o=json '.assets.registries[].auth' $HOME/.
 docker cp /home/vscode/.images.txt eda-demo-control-plane:/opt/images.txt
 docker exec eda-demo-control-plane sh -c "cat /opt/images.txt | xargs -P $(nproc) -I {} crictl pull --creds $TOKEN {}"
 
-make -f Makefile -f $TRY_EDA_OVERRIDES_FILE try-eda NO_KIND=yes NO_LB=yes KPT_SETTERS_FILE=$TRY_EDA_KPT_SETTERS_FILE
+make -f Makefile -f $TRY_EDA_OVERRIDES_FILE try-eda KPT_SETTERS_FILE=$TRY_EDA_KPT_SETTERS_FILE
