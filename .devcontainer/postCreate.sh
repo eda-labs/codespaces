b9edba45f3b4587e7551ee5ff58fc2fc83c8b846
hellt <dodin.roman@gmail.com>
2026-01-13 13:00:32 +0100

    add username

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -19,6 +19,6 @@ suffix=$(printf '%s' "$encoded" | base64 -d | cut -c 4- | tr -d '\n')
 TOKEN="${prefix}${suffix}"
 
 # preload images into the cluster from the EDA core list
-docker exec k3d-eda-demo-server-0 sh -c "cat /opt/images.txt | xargs -P $(nproc) -I {} crictl pull --creds $TOKEN {}"
+docker exec k3d-eda-demo-server-0 sh -c "cat /opt/images.txt | xargs -P $(nproc) -I {} crictl pull --creds nokia-eda-bot:$TOKEN {}"
 
 make -f Makefile -f $TRY_EDA_OVERRIDES_FILE try-eda NO_KIND=yes KPT_SETTERS_FILE=$TRY_EDA_KPT_SETTERS_FILE
