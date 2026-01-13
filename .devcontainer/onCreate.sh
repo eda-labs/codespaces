c67d6bb2619fe6b8547b5acf6c738898bc90e558
hellt <dodin.roman@gmail.com>
2026-01-13 18:45:58 +0100

    forward 9443 -> 443

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -14,5 +14,5 @@ k3d cluster create eda-demo \
     --image rancher/k3s:v1.34.1-k3s1 \
     --k3s-arg "--disable=traefik@server:*" \
     --volume "$HOME/.images.txt:/opt/images.txt@server:*" \
-    --port "9443:9443" \
+    --port "9443:443" \
     --port "9400-9410:9400-9410"
