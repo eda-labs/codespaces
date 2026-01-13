cccd979f251c06042ce755a3b382b707d0e19dbc
hellt <dodin.roman@gmail.com>
2026-01-13 12:52:29 +0100

    k3d with lb

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,8 +5,12 @@ cd $EDA_PLAYGROUND_DIR
 
 ensure-docker-is-ready
 
-# start the kind cluster
-make kind
+# start the k3d cluster
+k3d cluster create eda-demo \
+    --image rancher/k3s:v1.34.1-k3s1 \
+    --k3s-arg "--disable=traefik@server:*" \
+    --k3s-arg "--disable=servicelb@server:*" \
+    --volume "$HOME/.images.txt:/opt/images.txt@server:*" \
 
 # get token
 encoded=$(grep 'GH_PKG_TOKEN ?=' "Makefile" | sed 's/.*?= *//')
@@ -14,11 +18,7 @@ prefix=$(printf '%s' 'Z2hwCg==' | base64 -d)
 suffix=$(printf '%s' "$encoded" | base64 -d | cut -c 4- | tr -d '\n')
 TOKEN="${prefix}${suffix}"
 
-# preload images into kind cluster from the EDA core list
-docker cp /home/vscode/.images.txt eda-demo-control-plane:/opt/images.txt
-docker exec eda-demo-control-plane sh -c "cat /opt/images.txt | xargs -P $(nproc) -I {} crictl pull --creds nokia-eda-bot:$TOKEN {}"
+# preload images into the cluster from the EDA core list
+docker exec k3d-eda-demo-server-0 sh -c "cat /opt/images.txt | xargs -P $(nproc) -I {} crictl pull --creds $TOKEN {}"
 
-make -f Makefile -f $TRY_EDA_OVERRIDES_FILE try-eda KPT_SETTERS_FILE=$TRY_EDA_KPT_SETTERS_FILE
-
-# apply keycloak patch for codespaces redirects
-# make -f Makefile -f $TRY_EDA_OVERRIDES_FILE configure-codespaces-keycloak KPT_SETTERS_FILE=$TRY_EDA_KPT_SETTERS_FILE
+make -f Makefile -f $TRY_EDA_OVERRIDES_FILE try-eda NO_KIND=yes KPT_SETTERS_FILE=$TRY_EDA_KPT_SETTERS_FILE
