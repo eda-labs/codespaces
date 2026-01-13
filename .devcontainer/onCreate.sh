9569f6ec655b8c6b790a0f8ca6f9e1af529c9870
hellt <dodin.roman@gmail.com>
2026-01-13 17:02:39 +0100

    one more time bby

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -12,9 +12,7 @@ ensure-docker-is-ready
 # start the k3d cluster
 k3d cluster create eda-demo \
     --image rancher/k3s:v1.34.1-k3s1 \
+    --k3s-arg "--disable=traefik@server:*" \
     --volume "$HOME/.images.txt:/opt/images.txt@server:*" \
     --port "9443:9443" \
-    --port "9444:9444" \
-    --port "9445:9445" \
-    --port "9446:9446" \
-    --port "9447:9447"
+    --port "9400-9410:9400-9410"
