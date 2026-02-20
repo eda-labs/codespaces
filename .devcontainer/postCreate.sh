6a4b53464b583ee7bcd6877291a4243d24e578ad
Kaelem Chandra <kc@kaelem.net>
2026-02-21 10:12:46 +1300

    Remove token from prepull

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,18 +3,12 @@ source .devcontainer/utils.sh
 
 cd $EDA_PLAYGROUND_DIR
 
-# get token
-encoded=$(grep 'GH_PKG_TOKEN ?=' "Makefile" | sed 's/.*?= *//')
-prefix=$(printf '%s' 'Z2hwCg==' | base64 -d)
-suffix=$(printf '%s' "$encoded" | base64 -d | cut -c 4- | tr -d '\n')
-TOKEN="${prefix}${suffix}"
-
 log "Starting image prepull"
 T_START=$(date +%s)
 # preload images into the cluster from the EDA core list
 # to reduce the number of jobs: PARALLEL_JOBS=$(($(nproc) - 1))
 PARALLEL_JOBS=$(nproc)
-docker exec k3d-eda-demo-server-0 sh -c "cat /opt/images.txt | xargs -P $PARALLEL_JOBS -I {} crictl pull --creds nokia-eda-bot:$TOKEN {}"
+docker exec k3d-eda-demo-server-0 sh -c "cat /opt/images.txt | xargs -P $PARALLEL_JOBS -I {} crictl pull {}"
 
 T_END=$(date +%s)
 log "Images pulled. Took $((T_END-T_START)) seconds."
