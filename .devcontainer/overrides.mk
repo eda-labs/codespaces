a4f8dd8e6bf3e46e787d23b6739c17623a7c9bac
Kaelem Chandra <kc@kaelem.net>
2026-05-03 09:31:40 +1200

    Attempt simpler rootUrl patch

--- .devcontainer/overrides.mk
+++ .devcontainer/overrides.mk
@@ -1,7 +1,6 @@
 KPT_RETRY ?= 5
 KPT_RECONCILE_TIMEOUT ?= 3m
 KPT_LIVE_APPLY_ARGS += --reconcile-timeout=$(KPT_RECONCILE_TIMEOUT)
-EDA_PORT ?= 9443
 
 ifdef CODESPACE_EDA_VERSION
 EDA_CORE_VERSION=$(CODESPACE_EDA_VERSION)
@@ -53,6 +52,33 @@ patch-codespaces-engineconfig: | $(YQ) $(KPT_PKG) ## Patch the EngineConfig mani
 
 .PHONY: configure-try-eda-params
 configure-try-eda-params: | $(BASE) $(BUILD) $(KPT) $(KPT_SETTERS_TRY_EDA_FILE) patch-codespaces-engineconfig ## Configure parameters specific to try-eda
+
+.PHONY: ls-ways-to-reach-api-server
+ls-ways-to-reach-api-server: | $(KUBECTL) configure-codespaces-keycloak
+
+.PHONY: configure-codespaces-keycloak
+configure-codespaces-keycloak: | $(KUBECTL) ## Configure Keycloak realm rootUrl for GitHub Codespaces
+	@if [ -n "$(CODESPACE_NAME)" ] && [ -n "$(GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN)" ]; then \
+		CODESPACE_URL="https://$(CODESPACE_NAME)-$(EDA_PORT).$(GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN)" ;\
+		KC_URL="https://eda-api/core/httpproxy/v1/keycloak" ;\
+		echo "--> INFO: Configuring Keycloak realm rootUrl for Codespaces..." ;\
+		$(KUBECTL) wait --for=condition=ready pod -l eda.nokia.com/app=keycloak -n $(EDA_CORE_NAMESPACE) --timeout=300s ;\
+		TOKEN=$$($(KUBECTL) exec -n $(EDA_CORE_NAMESPACE) deploy/eda-toolbox -- curl -skf -X POST \
+			"$${KC_URL}/realms/master/protocol/openid-connect/token" \
+			-d "username=admin" -d "password=admin" -d "grant_type=password" -d "client_id=admin-cli" | jq -r '.access_token') ;\
+		$(KUBECTL) exec -n $(EDA_CORE_NAMESPACE) deploy/eda-toolbox -- sh -c "curl -skf \
+			-H 'Authorization: Bearer $${TOKEN}' \
+			'$${KC_URL}/admin/realms/eda' \
+			| jq '.attributes.rootUrl = \"$${CODESPACE_URL}/core/proxy/v1/identity\"' \
+			| curl -skf -X PUT \
+				'$${KC_URL}/admin/realms/eda' \
+				-H 'Authorization: Bearer $${TOKEN}' \
+				-H 'Content-Type: application/json' \
+				--data @-" ;\
+		echo "--> INFO: Keycloak realm rootUrl set to: $${CODESPACE_URL}/core/proxy/v1/identity" ;\
+	else \
+		echo "--> INFO: Not running in Codespaces, skipping Keycloak realm rootUrl configuration" ;\
+	fi
 
 ifdef NO_TOPO
 TOPO := $(TOPO_EMPTY)
